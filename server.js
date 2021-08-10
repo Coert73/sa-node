723f8f79fdeecd99ef44bfff58389932dc16ff83
Kyle Oosthuizen <koosthuizen@retrorabbit.co.za>
2021-08-10 19:21:59 +0200

    Renamed variables

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const cors = require("cors");
 const app = express();
 
 //import the routes
-const todoRoutes = require('./routes/appointments');
+const appointmentRoutes = require('./routes/appointments');
 
-app.use(todoRoutes);
+app.use(todappointmentRoutesoRoutes);
 app.use(express.json());
 app.use(cors());
 
@@ -13,6 +13,6 @@ app.use(cors());
 const host = '127.0.0.1';
 const port = 8081;
 
-var server = app.listen(port, function () {
+app.listen(port, function () {
   console.log("Example app listening at http://%s:%s", host, port)
 })
